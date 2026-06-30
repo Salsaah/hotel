87a898865911d442b409987f13a812afebacb8b9
salsabila <salsabilaah655@gmail.com>
2026-06-30 12:03:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+new Swiper(".heroSwiper",{
+
+    loop:true,
+
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+
+    pagination:{
+        el:".heroSwiper .swiper-pagination",
+        clickable:true,
+    }
+
+});
+
+new Swiper(".sec1Swiper", {
+    loop:true,
+
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+
+    pagination:{
+        el:".sec1Swiper .swiper-pagination",
+        clickable:true,
+    }
+});
+
+
+new Swiper(".sec4Swiper1", {
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+new Swiper(".sec4Swiper2", {
+    loop: true,
+    autoplay: {
+        delay: 3000, // boleh beda biar lebih hidup
+        disableOnInteraction: false,
+    },
+});
